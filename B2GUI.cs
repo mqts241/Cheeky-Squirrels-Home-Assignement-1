07f832c66b7aea015d7d3866dbbff883ce55bb31
Gabbo693 <gabrielesolazzo43@gmail.com>
2025-02-26 16:40:41 +0100

    First implementation of GUI

---
+++ B2GUI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.PropertyStore;
+using Avalonia.Layout;
+
+namespace Smile;
+
+public class B2GUI
+{
+    public Window win;
+
+    public B2GUI()
+    {
+        win = new Window
+        {
+            Title = "B2Image Editor",
+            Height = 600,
+            Width = 400,
+        };
+
+        var stack = new StackPanel {
+            Orientation = Orientation.Vertical,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center,
+            Margin = new Thickness(25),
+        };
+
+        var loadButton = new Button {
+            Content = "Load File",    
+        };
+        loadButton.Click += LoadFile;
+
+        var saveButton = new {
+            Content = "Save File",
+        };
+        saveButton.Click += SaveFile;
+
+        stack.Children.Add(loadButton);
+        stack.Children.Add(saveButton);
+
+        win.Content = stack;
+        win.Show();
+    }
+
+    private void LoadFile(object? sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Load button clicked.");
+            // Implement file loading logic here
+        }
+
+    private void SaveFile(object? sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Save button clicked.");
+            // Implement file saving logic here
+        }
+}
---
+++ Project.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Smile;
+
+class Project
+{
+    public static void Main(string[] args)
+    {
+        AppBuilder.Configure<Application>()
+                  .UsePlatformDetect()
+                  .Start(AppMain, args);
+    }
+
+    public static void AppMain(Application app, string[] args)
+    {
+        app.Styles.Add(new Avalonia.Themes.Fluent.FluentTheme());
+        app.RequestedThemeVariant = Avalonia.Styling.ThemeVariant.Light;
+
+	    var gui = new B2GUI();
+        app.Run(gui.win);
+    }
+}
